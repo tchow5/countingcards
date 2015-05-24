0d6be9df55b8036294307608176765d2ce4e4727
tchow5 <timchow26@yahoo.com>
2015-05-24 02:04:30 -0400

    stuff works

--- card.js
+++ card.js
@@ -1,3 +1,26 @@
 var number = 0;
-var randomInt = 0; 
+var randomInt = 0;
+
+var card1 = "1.png";
+
+// $(document).ready(function(){
+// 	$('#instructions').hide();
+// 	$('.button').click(function(){
+// 		$('#enterbody').hide();
+// 		$('#instructions').show();
+// 	});
+// });
+
+$(document).ready(function(){
+	$('#instructions').hide();
+    $('#cards').hide();
+    $(".button").click(function(){
+        $("#enterbody").hide();
+    });
+    $(".button").click(function(){
+        $("#instructions").show();
+    });
+});
+
+
 
