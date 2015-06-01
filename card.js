95097deb80a0fd9ec6320ee30d3c8f65e8844e23
tchow5 <timchow26@yahoo.com>
2015-05-31 20:04:50 -0400

    shuffling

--- card.js
+++ card.js
@@ -34,19 +34,36 @@ $(document).ready(function(){
    	 });
 
 });
+//they dont do anything yet!!!
+// function FindCard(players){
 
-function FindCard(players){
-
-	currentcard = Math.floor((Math.random() * 52) + 1);
+// 	currentcard = Math.floor((Math.random() * 52) + 1);
 
 
+// }
+
+// function Timeout(){
+
+// 	$("#cardtext p").hide();
+
+// }
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
-
-function Timeout(){
-
-	$("#cardtext p").hide();
-
-}
-
-
 
